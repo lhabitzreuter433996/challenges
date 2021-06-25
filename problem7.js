1aae981adf786f752717f116d7381e56c833ebba
LEONARDO HABITZREUTER <leonardo.habitzreuter@invillia.com>
2021-06-25 11:23:03 -0300

    performance improvement

--- problem7.js
+++ problem7.js
@@ -2,10 +2,12 @@ let targetNumber = 0
 let currentPosition = 6
 
 const isPrimeNumber = number => {
-  for(var i = 2; i < number; i++) {
+  if(number % 2 === 0) return false
+
+  for(var i = 3; i < number; i = i + 2) {
     if(number % i === 0) return false
   }
-  return number > 1
+  return true
 }
 
 for (let index = 15; currentPosition !== 10001; index = index + 2) {
